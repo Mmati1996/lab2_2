955ea9b854b1155f405d2d182fc4cd62eea36150
Mmati1996 <57298066+Mmati1996@users.noreply.github.com>
2020-03-28 20:28:42 +0100

    state test for identical, different and similar sequences

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
@@ -10,7 +10,9 @@ class SimilarityFinderStateTest {
     SearchMock mock;
     int[] empty1= new int[]{};
     int[] empty2= new int[]{};
-    int[] sequence = new int[]{3,4,5};
+    int[] sequence1 = new int[]{3,4,5,6};
+    int[] sequence2 = new int[]{31,44,267,12};
+    int[] sequence3 = new int[]{3,4,6,6};
 
     @BeforeEach
     void init(){
@@ -26,12 +28,27 @@ class SimilarityFinderStateTest {
 
     @Test
     void firstEmptySequencesTest(){
-        assertTrue(sf.calculateJackardSimilarity(sequence,empty2) == 0);
+        assertTrue(sf.calculateJackardSimilarity(sequence1,empty2) == 0);
     }
 
     @Test
     void secondEmptySequencesTest(){
-        assertTrue(sf.calculateJackardSimilarity(empty1,sequence) == 0);
+        assertTrue(sf.calculateJackardSimilarity(empty1, sequence1) == 0);
+    }
+
+    @Test
+    void identicalSequencesTest(){
+        assertTrue(sf.calculateJackardSimilarity(sequence1, sequence1) == 1);
+    }
+
+    @Test
+    void absolutelyDifferentSequencesTest(){
+        assertTrue(sf.calculateJackardSimilarity(sequence1, sequence2) == 0);
+    }
+
+    @Test
+    void similarSequencesTest(){
+        assertTrue(sf.calculateJackardSimilarity(sequence1, sequence3) > 0.5);
     }
 
 
