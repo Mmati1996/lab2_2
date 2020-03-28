22539a815955cc627d8f0fff084f9e2f41b1f879
Mmati1996 <57298066+Mmati1996@users.noreply.github.com>
2020-03-28 19:23:21 +0100

    Behaviour tests checking if null argument throws exception

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,7 +1,33 @@
 package edu.iis.mto.similarity;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityFinderTest {
+
+    SimilarityFinder sf;
+    int[] sequence = new int[]{1,3,7,12};
+
+    @Test
+    void nullsTest(){
+        assertThrows(NullPointerException.class,()->{
+           sf.calculateJackardSimilarity(null,null);
+        });
+    }
+
+    @Test
+    void firstNullTest(){
+        assertThrows(NullPointerException.class,()->{
+           sf.calculateJackardSimilarity(sequence,null);
+        });
+    }
+
+    @Test
+    void secondNullTest(){
+        assertThrows(NullPointerException.class,()->{
+            sf.calculateJackardSimilarity(null,sequence);
+        });
+    }
 
 }
